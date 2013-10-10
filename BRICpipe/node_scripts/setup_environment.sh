997e7ab399f7316e07893dee27d4f862bb72c20c
Andreas Glatz <a.glatz@sms.ed.ac.uk>
2013-10-10 11:57:34 +0200

    Backup before restart!

--- BRICpipe/node_scripts/setup_environment.sh
+++ BRICpipe/node_scripts/setup_environment.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-SLC_PFX=/usr/local/Slicer3/
-FSL_PFX=/usr/local/fsl/4.1.9/
-FLIRT_PASS1="-dof 6"
+SLC_PFX=/media/LP3TBdisk/Andreas_PhD/Slicer3
+FSL_PFX=/usr/local/fsl
+FLIRT_PASS1="-dof 6 -cost normmi -searchcost normmi"
 FLIRT_PASS2=""
 AR_IDX="1"
-WM_IDX="3"
-GM_IDX="4"
-CS_IDX="1"
-BET_GRE="-f 0.4"
-BET_T1W="-f 0.25 -B"
-BET_T2W="-f 0.25 -B"
+WM_IDX="4"
+GM_IDX="3"
+CS_IDX="2"
+BET_GRE="-f 0.3"
+BET_T1W="-f 0.3 -B"
+BET_T2W="-f 0.3 -B"
 BET_FLAIR="-f 0.25 -B"
 
 # Return environment variable name
--- libBRIC/mineral-deposit-segmentation/combine_subjectdata.m
+++ libBRIC/mineral-deposit-segmentation/combine_subjectdata.m
@@ -6,17 +6,35 @@ addpath('NIFTI/');
 
 close all; clear all; clc;
 
-sub_file = 'subjects_98.csv'; % csv file
-mat_name = 'subjects_98_cs.mat'; % matlab file for saving results
+anot_file = '/media/LP3TBdisk/Andreas_PhD/mineral/annot.csv';
+base_path = '/media/LP3TBdisk/Andreas_PhD/mineral-deposit-segmentation-pipeline/BRICpipe';
+base_name = 'subjects_asps_318_checked_both_t2w';
+sub_file = fullfile(base_path, [base_name '.csv']);
+mat_name = [base_name '_blob.mat']; % matlab file for saving results
 t2swhypo_name = 'T2swHypo_mask'; % name of T2*w hypointensity mask subject directory
 t2swhypot1whypo_name = 'T2swHypoT1whypo_mask';
 t2swhypot1whyper_name = 'T2swHypoT1whyper_mask';
-roi_name = 'RO_mask'; % name of ROI mask in subject directory
-gre_name = 'GRE_brain_restore'; % name of GRE volume
+ntis_name = 'NormTis_mask';
+roi_name = 'RO_weinew_mask'; % name of ROI mask in subject directory
+t2sw_name = 'GRE_brain_restore'; % name of GRE volume
+t1w_name = 'T1W_brain_restore';
+t2w_name = 'T2W_brain_restore';
+r2s_name = 'R2s';
+r2_name = 'R2';
+pha_name = 'GRE_phu';
+pha_valid_name = 'GRE_sos_bin';
 icv_name = 'GRE_brain_mask'; % name of ICV mask
 
 % Read subject file
 [Subjects, N_total] = get_subject_dirs(sub_file);
+
+% Read annot file
+fd = fopen(anot_file);
+SubjAnot = textscan(fd, '%s%s%s%s%s%s%*[^\n]', ...
+    'delimiter',',',...
+    'treatAsEmpty',{'NA','na'}, ...
+    'commentStyle', '#');
+fclose(fd);
 
 % Init result structure
 Ret = struct;
@@ -26,16 +44,20 @@ for i=1:N_total
     Ret(ret_idx).Path = path;
     fprintf('Reading subject data for subject %s...', path);
     
-    S_fe = load_series(fullfile(path, t2swhypo_name), []);
-    N_fe = sum(logical(S_fe(:)));
+    S_feat = load_series(fullfile(path, t2swhypo_name), []);
+    N_fe = sum(logical(S_feat(:)));
     fprintf('%d voxels...', N_fe);
     
+    % Find corresponding annotation (chi number that matches path)
+    f = @(in) (~isempty(strfind(path, in)));
+    M_subj = cellfun(f, SubjAnot{1});
+    
     % Skip empty masks
-    if N_fe > 0
+    if N_fe > 0 && sum(M_subj) == 1
         % Get connected component statistics
         [Ret(ret_idx).CC] = ...
             conncomp_stats(path, t2swhypo_name, 0, roi_name, 1, 1, ...
-                           gre_name, t2swhypot1whypo_name, t2swhypot1whyper_name);
+                           t2sw_name, t2swhypot1whypo_name, t2swhypot1whyper_name);
 
         % Labels of ROI mask
         S_roi = load_series(fullfile(path, roi_name), []);
@@ -43,15 +65,100 @@ for i=1:N_total
         Ret(ret_idx).Lab_roi = Lab_roi(2:end); % creates list of roi labels
         
         % Labels of ID mask
-        S_fe = load_series(fullfile(path, t2swhypo_name), []);
-        Ret(ret_idx).Lab = S_fe(logical(S_fe))'; % creates a line vector
+        S_feat = load_series(fullfile(path, t2swhypo_name), []);
+        Ret(ret_idx).Lab = S_feat(logical(S_feat))'; % creates a line vector
         
         % ICV
         SM_brain = logical(load_series(fullfile(path, icv_name), []));
-        NII_brain = load_series(fullfile(path, icv_name), 0);
-        Ret(ret_idx).ICV = get_volume(SM_brain, ...
-                                NII_brain.hdr.dime.pixdim(2:4));
-                            
+        Ret(ret_idx).ICV = str2double(SubjAnot{5}{M_subj});
+        if Ret(ret_idx).ICV <= 0 || isnan(Ret(ret_idx).ICV)
+            NII_brain = load_series(fullfile(path, icv_name), 0);
+            Ret(ret_idx).ICV = get_volume(SM_brain, ...
+                                          NII_brain.hdr.dime.pixdim(2:4));
+        end
+        
+        % Age
+        Ret(ret_idx).Age = str2double(SubjAnot{2}{M_subj});
+        
+        % Gender
+        Ret(ret_idx).Male = strcmp(SubjAnot{6}{M_subj}, 'm');
+        Ret(ret_idx).Female = strcmp(SubjAnot{6}{M_subj}, 'w');
+
+        % T2sw Intensity info
+        S_t2sw = single(load_series(fullfile(path, t2sw_name), []));
+        S_ntis = single(load_series(fullfile(path, ntis_name), []));
+        N_lab = length(Ret(ret_idx).Lab_roi);
+        for lab_idx = 1:N_lab
+            SM_ntis = S_ntis == Ret(ret_idx).Lab_roi(lab_idx);
+            SM_feat = S_feat == Ret(ret_idx).Lab_roi(lab_idx);
+            SM_valid = S_t2sw > 0 & SM_brain;
+            Ret(ret_idx).T2sw(lab_idx) = get_tis_ints(S_t2sw, SM_ntis, SM_feat, SM_valid);
+        end
+        Ret(ret_idx).T2swValid = 1;
+        
+        % T1w Intensity info
+        S_t1w = single(load_series(fullfile(path, t1w_name), []));
+        N_lab = length(Ret(ret_idx).Lab_roi);
+        for lab_idx = 1:N_lab
+            SM_ntis = S_ntis == Ret(ret_idx).Lab_roi(lab_idx);
+            SM_feat = S_feat == Ret(ret_idx).Lab_roi(lab_idx);
+            SM_valid = S_t1w > 0 & SM_brain;
+            Ret(ret_idx).T1w(lab_idx) = get_tis_ints(S_t1w, SM_ntis, SM_feat, SM_valid);
+        end
+        Ret(ret_idx).T1wValid = 1;
+        
+        % T2w Intensity info
+        S_t2w = single(load_series(fullfile(path, t2w_name), []));
+        N_lab = length(Ret(ret_idx).Lab_roi);
+        for lab_idx = 1:N_lab
+            SM_ntis = S_ntis == Ret(ret_idx).Lab_roi(lab_idx);
+            SM_feat = S_feat == Ret(ret_idx).Lab_roi(lab_idx);
+            SM_valid = S_t2w > 0 & SM_brain;
+            Ret(ret_idx).T2w(lab_idx) = get_tis_ints(S_t2w, SM_ntis, SM_feat, SM_valid);
+        end
+        Ret(ret_idx).T2wValid = 1;
+        
+        % R2s Intensity info
+        S_r2s = single(load_series(fullfile(path, r2s_name), []));
+        S_r2s = S_r2s(:, :, :, 1);
+        N_lab = length(Ret(ret_idx).Lab_roi);
+        for lab_idx = 1:N_lab
+            SM_ntis = S_ntis == Ret(ret_idx).Lab_roi(lab_idx);
+            SM_feat = S_feat == Ret(ret_idx).Lab_roi(lab_idx);
+            SM_valid = S_r2s > 0 & SM_brain;
+            Ret(ret_idx).R2s(lab_idx) = get_tis_ints(S_r2s, SM_ntis, SM_feat, SM_valid);
+        end
+        Ret(ret_idx).R2sValid = 1;
+        
+        % R2 Intensity info
+        S_r2 = single(load_series(fullfile(path, r2_name), []));
+        N_lab = length(Ret(ret_idx).Lab_roi);
+        for lab_idx = 1:N_lab
+            SM_ntis = S_ntis == Ret(ret_idx).Lab_roi(lab_idx);
+            SM_feat = S_feat == Ret(ret_idx).Lab_roi(lab_idx);
+            SM_valid = S_r2 > 0 & SM_brain;
+            Ret(ret_idx).R2(lab_idx) = get_tis_ints(S_r2, SM_ntis, SM_feat, SM_valid);
+        end
+        Ret(ret_idx).R2Valid = 1;
+        
+        try
+            % Pha Intensity info
+            S_pha = single(load_series(fullfile(path, pha_name), []));
+            S_pha = S_pha(:, :, :, 3);
+            SM_pha_valid = logical(load_series(fullfile(path, pha_valid_name), []));
+            N_lab = length(Ret(ret_idx).Lab_roi);
+            for lab_idx = 1:N_lab
+                SM_ntis = S_ntis == Ret(ret_idx).Lab_roi(lab_idx);
+                SM_feat = S_feat == Ret(ret_idx).Lab_roi(lab_idx);
+                SM_valid = SM_pha_valid & SM_brain;
+                Ret(ret_idx).Pha(lab_idx) = get_tis_ints(S_pha, SM_ntis, SM_feat, SM_valid);
+            end
+            Ret(ret_idx).PhaValid = 1;
+        catch
+            Ret(ret_idx).Pha = Ret(ret_idx).R2;
+            Ret(ret_idx).PhaValid = 0;
+        end
+            
         ret_idx = ret_idx + 1;
     end
     
