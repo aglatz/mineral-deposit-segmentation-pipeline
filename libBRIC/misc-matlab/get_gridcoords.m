1d0f3705892dddb6c71d3b9ed74220095dde6526
Andreas Glatz <a.glatz@sms.ed.ac.uk>
2013-10-10 10:52:41 +0200

    Bugfix: Now the gridcoords should be correct

--- libBRIC/misc-matlab/get_gridcoords.m
+++ libBRIC/misc-matlab/get_gridcoords.m
@@ -11,11 +11,11 @@ X_old = F_old(1):F_old(1):(size(S_old, 1)*F_old(1));
 Y_old = F_old(2):F_old(2):(size(S_old, 2)*F_old(2));
 Z_old = F_old(3):F_old(3):(size(S_old, 3)*F_old(3));
 % New lattice coordinates
-X_new = F_old(1):F_new(1):(size(S_old, 1)*F_old(1));
-X_new = F_old(1)+(max(X_old)-max(X_new))/2:F_new(1):(size(S_old, 1)*F_old(1));
+X_new = F_new(1):F_new(1):(size(S_old, 1)*F_old(1));
+X_new = (max(X_old)-max(X_new))/2:F_new(1):(size(S_old, 1)*F_old(1));
 %
-Y_new = F_old(2):F_new(2):(size(S_old, 2)*F_old(2));
-Y_new = F_old(2)+(max(Y_old)-max(Y_new))/2:F_new(2):(size(S_old, 2)*F_old(2));
+Y_new = F_new(2):F_new(2):(size(S_old, 2)*F_old(2));
+Y_new = (max(Y_old)-max(Y_new))/2:F_new(2):(size(S_old, 2)*F_old(2));
 %
-Z_new = F_old(3):F_new(3):(size(S_old, 3)*F_old(3));
-Z_new = F_old(3)+(max(Z_old)-max(Z_new))/2:F_new(3):(size(S_old, 3)*F_old(3));
+Z_new = F_new(3):F_new(3):(size(S_old, 3)*F_old(3));
+Z_new = (max(Z_old)-max(Z_new))/2:F_new(3):(size(S_old, 3)*F_old(3));
