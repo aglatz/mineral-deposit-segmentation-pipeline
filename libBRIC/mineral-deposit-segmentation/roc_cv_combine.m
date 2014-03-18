8ed94ecb54c2efd7eda76a94fee6f8c562942d9d
Andreas Glatz <a.glatz@sms.ed.ac.uk>
2014-03-18 12:18:36 +0000

    - Adapted to fit to new version of segmentation algorithm with 'dR2s_thr'

--- libBRIC/mineral-deposit-segmentation/roc_cv_combine.m
+++ libBRIC/mineral-deposit-segmentation/roc_cv_combine.m
@@ -3,34 +3,37 @@ addpath ../../libBRIC/misc-matlab/
 close all; clear all; clc
 
 SubjectFile = 'subjects_98';
-Pfx = 'ad_t1w0.975';
+Pfx = 'ad_t1w_mineral_thr14_roi'; % varies the T1w hypointensity factor
 AdaptiveFlag = true;
 RoiLabelTable = {[13, 11, 12, 14]};
 N_cpus = 6;
-Thr = (0:0.1:0.9)';
+Thr = 0.3:0.1:1; %12:0.5:16; %9:18;
+N_thr = length(Thr);
 N_cv = 10;
 out = struct;
 
 H2 = figure;
 Col = hsv(N_cv);
+J_mean = NaN(N_cv, N_thr);
 for idx_dir = 1:N_cv
-    N_thr = length(Thr);
-    J_mean = zeros(N_thr, 1);
+    if idx_dir == 6
+        fprintf('');
+    end
     
     for idx_thr = 1:N_thr
         [~, J] = load_matdata(fullfile(num2str(idx_dir), ['hu_' num2str(Thr(idx_thr)) '_' Pfx '.mat']));
-        J_mean(idx_thr) = quantile(J, .5);
+        J_mean(idx_dir, idx_thr) = quantile(J, .5);
     end    
 
-	idx = find(J_mean == max(J_mean));
+	idx = find(J_mean(idx_dir, :) == max(J_mean(idx_dir, :)));
     [idx, len] = find_largemax(idx, '');
     if mod(len, 2) == 0
         idx = [idx-1, idx];
     end
 
-    figure(H2); hold on; plot(Thr, J_mean, 'color', Col(idx_dir, :));
+    figure(H2); hold on; plot(Thr, J_mean(idx_dir, :), 'color', Col(idx_dir, :));
     out(idx_dir).thr = mean(Thr(idx));
-    scatter(out(idx_dir).thr, mean(J_mean(idx)), 'k', 'filled');
+    scatter(out(idx_dir).thr, mean(J_mean(idx_dir, idx)), 'k', 'filled');
     
 %     SubjectFile_traintest = [SubjectFile '_' Pfx '_' num2str(idx_dir)];
 %     [out(idx_dir).Ret, out(idx_dir).Subjects] = ...
@@ -40,12 +43,17 @@ for idx_dir = 1:N_cv
 %                                   'ReportName', 'class', ...
 %                                   'AdaptiveFlag', AdaptiveFlag, ...
 %                                   'SaveMaskFlag', true, ...
-%                                   'IntvarP', out(idx_dir).thr);
+%                                   'TE_gre', 15e-3, ...
+%                                   'dR2s_thr', out(idx_dir).thr);
 end
 
-MatFileName = [SubjectFile '_' Pfx '.mat'];
-save(MatFileName, 'out');
+% MatFileName = [SubjectFile '_' Pfx '.mat'];
+% save(MatFileName, 'out');
 
 figure(H2);
+plot(Thr, quantile(J_mean, .5), 'k', 'LineWidth', 2);
+plot(Thr, quantile(J_mean, .75), '--k', 'LineWidth', 2);
+plot(Thr, quantile(J_mean, .25), '--k', 'LineWidth', 2);
 xlabel('\bf Cumulative probability');
 ylabel('\bf Jaccard index');
+
