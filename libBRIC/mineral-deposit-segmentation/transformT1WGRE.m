0bc38cb778ba8834796ee72becfe2ade1c5b47aa
Andreas Glatz <a.glatz@sms.ed.ac.uk>
2014-03-27 12:18:20 +0000

    - Adapted code to be able to handle new version of segment_us_single() - Now generates CNR vs concentration plots, Size vs concentration plots and boxplots showing how many beads were found and the average segmentation threshold of for these beads.

--- libBRIC/mineral-deposit-segmentation/transformT1WGRE.m
+++ libBRIC/mineral-deposit-segmentation/transformT1WGRE.m
@@ -1,4 +1,4 @@
-function [out1, out2] = transformT1WGRE(f, Subject, adapt_flag, te)
+function [out1, out2] = transformT1WGRE(Subject, adapt_flag, te, intstd_thr)
 
 % This script processes phantom data. It can remove the appearance of CaAlg
 % balls from T1w and replace them with intensities from surrounding agar.
@@ -37,7 +37,8 @@ RoiLabelTable = {1, 2, 3, 11, 12, 13, 21, 22, 23};
 % S_t1w_new = 400 + 50*randn(size(S_t1w));
 % save_series([Subject '/T1W_brain_restore_orig'], [Subject '/T1W_brain_restore'], S_t1w_new, []);
 
-segment_us_single(Subject, RoiLabelTable, 'class', 1, [f 0], adapt_flag, te, 0);
+Ret = segment_us_single(Subject, RoiLabelTable, 'class', 1, [1 0], adapt_flag, te, 0, 0.1, intstd_thr);
+I_thr = [Ret.I_thr{:}]; % first row: gre thr
 
 S_roi = load_series([Subject '/RO_mask.nii.gz'], []);
 S_norm = load_series([Subject '/NormTis_mask.nii.gz'], []);
@@ -48,20 +49,21 @@ Dim = 3;
 % L_hypo = conncomp_init(SM_hypo, Dim);
 SM_hypo_ref = logical(load_series([Subject '/FE_roi_mask.nii.gz'], []));
 L_hypo_ref = conncomp_init(SM_hypo_ref, Dim);
+%save_series([Subject '/FE_roi_mask.nii.gz'], [Subject '/FE_roi_mask_cc.nii.gz'], L_hypo_ref, []);
 
 S_gre = single(load_series([Subject '/GRE_brain_restore'], []));
 fd = fopen([Subject '/GRE_brain_restore_mask.txt'], 'r');
 Noise_gre = single(fscanf(fd, '%d'));
 fclose(fd);
 
-S_t1w = single(load_series([Subject '/T2w_brain_restore'], []));
-fd = fopen([Subject '/T2w_brain_restore_mask.txt'], 'r');
+S_t1w = single(load_series([Subject '/T1w_brain_restore'], []));
+fd = fopen([Subject '/T1w_brain_restore_mask.txt'], 'r');
 Noise_t1w = single(fscanf(fd, '%d'));
 fclose(fd);
 
-Lab = cell2mat(RoiLabelTable);
-out1 = NaN(length(Lab), 3);
-out2 = NaN(length(Lab), 8, 3);
+Lab = [1,2,3,11,12,13,21,22,23]; % dont change order
+out1 = NaN(length(Lab), 5);
+out2 = NaN(length(Lab), 7, 4);
 for idx_lab = 1:length(Lab)
 	SM_roi = S_roi == Lab(idx_lab);
     SM_norm = S_norm == Lab(idx_lab);
@@ -70,55 +72,90 @@ for idx_lab = 1:length(Lab)
     out1(idx_lab, 2) = length(Lab_hypo);
     Lab_hypo_ref = unique(L_hypo_ref(SM_roi)); Lab_hypo_ref = Lab_hypo_ref(Lab_hypo_ref > 0);
     out1(idx_lab, 3) = length(Lab_hypo_ref);
-    for idx2_lab = 1:length(Lab_hypo_ref)
+    for idx2_lab = 1:7 %length(Lab_hypo_ref)
         SM_tmp = L_hypo_ref == Lab_hypo_ref(idx2_lab);
         % position
         [~, ~, z] = ind2sub(size(SM_tmp), find(SM_tmp));
         out2(idx_lab, idx2_lab, 1) = median(z);
         % gre contrast
-        out2(idx_lab, idx2_lab, 2) = abs( median(S_gre(L_hypo_ref == Lab_hypo_ref(idx2_lab))) - ...
+        out2(idx_lab, idx2_lab, 2) = abs( median(S_gre(SM_tmp)) - ...
                                           median(S_gre(SM_norm))) / Noise_gre;
         if (~sum(Lab_hypo == Lab_hypo_ref(idx2_lab)))
             out2(idx_lab, idx2_lab, 2) = out2(idx_lab, idx2_lab, 2)*-1;
         end
         % t1w contrast
-        out2(idx_lab, idx2_lab, 3) = abs( median(S_t1w(L_hypo_ref == Lab_hypo_ref(idx2_lab))) - ...
+        out2(idx_lab, idx2_lab, 3) = abs( median(S_t1w(SM_tmp)) - ...
                                           median(S_t1w(SM_norm))) / Noise_t1w;
         if (~sum(Lab_hypo == Lab_hypo_ref(idx2_lab)))
             out2(idx_lab, idx2_lab, 3) = out2(idx_lab, idx2_lab, 3)*-1;
         end
+        % size
+        out2(idx_lab, idx2_lab, 4) = get_volume(SM_tmp, F);
     end
+    out1(idx_lab, 5) = abs(I_thr(1, idx_lab) - median(S_gre(SM_norm))) / Noise_gre;
+end
+
+for idx_lab = 1:3:length(Lab)
+    SM_roi = S_roi == Lab(idx_lab);
+    for idx1 = 1:2
+        SM_roi = SM_roi | S_roi == Lab(idx_lab+idx1);
+    end
+    Ret = validate_raw(SM_roi & SM_hypo, SM_roi & SM_hypo_ref, [], F);
+    out1(idx_lab, 4) = Ret.Jaccard;    
 end
 
 fprintf('Label Selected Ref\n');
-out1
+Tmp=out1(1:3, :); [quantile(Tmp, .5); iqr(Tmp)/ 1.349]
+Tmp=out1(4:6, :); [quantile(Tmp, .5); iqr(Tmp)/ 1.349]
+Tmp=out1(7:9, :); [quantile(Tmp, .5); iqr(Tmp)/ 1.349]
 csvwrite([Subject '/out1.csv'], out1);
 out2
 csvwrite([Subject '/out2.csv'], out2);
 fprintf('------------------\n');
 
 figure;
-for idx1 = 1:2
-    subplot(1,2,idx1);
+subplot(3,1,1);
+boxplot(out1(:, [2]),[1 1 1 2 2 2 3 3 3], 'labels', {'N/1200nm+HA', 'N/250nm', 'N/1200nm'});
+ylabel('Count');
+subplot(3,1,2);
+boxplot(out1(:, [4]),[1 1 1 2 2 2 3 3 3], 'labels', {'N/1200nm+HA', 'N/250nm', 'N/1200nm'});
+ylabel('Jaccard index');
+subplot(3,1,3);
+boxplot(out1(:, [5]),[1 1 1 2 2 2 3 3 3], 'labels', {'N/1200nm+HA', 'N/250nm', 'N/1200nm'});
+ylabel('Threshold (CNR)');
+
+conc = [7 5 4 3 2 1 0];
+for idx1 = 1:3
+    figure
     hold on;
     C = hsv(3);
     L = cell(3, 1);
     for idx = 1:3
-        Y = median(abs(out2(1:3+(idx-1)*3, :, 2+idx1-1)));
-        Y_std = iqr(abs(out2(1:3+(idx-1)*3, :, 2+idx1-1))) ./ 1.349;
-        errorbar(Y, Y_std, 'color', C(idx, :));
+        Tmp = abs(out2(1:3+(idx-1)*3, :, 2+idx1-1));
+        if idx1 == 3
+            Tmp = (Tmp*3/4/pi).^(1/3);
+        end
+        Y = median(Tmp);
+        Y_std = iqr(Tmp) ./ 1.349;
+        errorbar(conc, Y(1:length(conc)), Y_std(1:length(conc)), 'color', C(idx, :));
         L{idx} = sprintf('%d,', out1(1+(idx-1)*3, 1));
     end
-    legend(L);
-    for idx = 1:3
-        M_tmp = out2(1:3+(idx-1)*3, :, 2+idx1-1) < 0;
-        H_tmp = out2(1:3+(idx-1)*3, :, 2+idx1-1);
-        T_tmp = quantile(abs(H_tmp(M_tmp)), .95);
-        plot([1 8], [T_tmp T_tmp], 'color', C(idx, :));
-        fprintf('%s: %0.2f\n', L{idx}, T_tmp);
+    legend('N/1200nm+HA', 'N/250nm', 'N/1200nm');
+%     if idx1 == 1
+%         for idx = 1:3
+%             Thr = median(out3(1:3+(idx-1)*3));
+%             plot(conc([1 end]), [Thr, Thr], 'color', C(idx, :));
+%             fprintf('%s: %0.2f\n', L{idx}, Thr);
+%         end
+%     end
+    xlabel('\bf Concentration in mmol/l');
+    set(gcf, 'color', 'white');
+    if idx1 == 3
+        ylabel('\bf Volume in mm^3');
+    else
+        ylabel('\bf Contrast to noise ratio');
+        axis([0     8     0     10]);
     end
-    xlabel('\bf Bead number');
-    ylabel('\bf Contrast to noise ratio');
 end
 fprintf('------------------\n');
 
